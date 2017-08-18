5e2ff3e1fcc84f70f5d2fb0863ef0101d5cda97a
PengfeiGuo <626138959@qq.com>
2017-08-18 18:00:33 +0800

    拆分loading与错误处理拦截器

--- frontend/src/js/common/interceptors.js
+++ frontend/src/js/common/interceptors.js
@@ -1,47 +1,36 @@
 import Vue from 'vue';
 import { Indicator, Toast } from 'mint-ui';
-import Cache from './cache.js';
 
 export default [
 
-  // 1、添加请求loading和错误处理
+  // 统一loading
   function(req, next) {
 
     // 请求前添加loading
     Indicator.open('加载中...');
 
     // 请求后取消loading
-    next(rep => {
+    next(function(rep) {
       Indicator.close();
+    });
+  },
 
-      // 请求失败进行统一的错误处理
-      if(!rep.ok || rep.body.status != 0) {
+  // 统一错误处理
+  function(req, next) {
+
+    next(function(rep) {
+
+      // 请求本身错误，给出提示
+      if(!rep.ok) {
         Toast('请求失败，请稍候再试');
+      }
+
+      // 服务器错误，给出提示，并阻止成功回调的执行
+      else if(rep.body.status != 0) {
+        rep.ok = false;
+        Toast('服务器错误，请联系客服人员');
       }
     });
   }
 
-  // 2、请求数据时优先使用缓存，缓存到期或者没有再请求接口
-//function(req, next) {
-
-//  let url = req.url;
-//  if(Cache[url]) {
-//
-//  }else {
-//
-//  }
-//
-//  // 请求后取消loading
-//  next(rep => {
-//    Indicator.close();
-//
-//    // 请求失败进行统一的错误处理
-//    if(!rep.ok || rep.body.status != 0) {
-//      Toast('请求失败，请稍候再试');
-//    }
-//  });
-//}
-
-
 ];
-
