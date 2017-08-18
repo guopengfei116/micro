e9820564a62724c1d609d540faea244acebd1da9
PengfeiGuo <626138959@qq.com>
2017-08-19 01:12:40 +0800

    fix-http模块因无法正确分析params造成的缓存数据错误；同时http模块改用缓存数据副本，防止数据被vue及后续操作带来的缓存数据污染，

--- frontend/src/js/api/http.js
+++ frontend/src/js/api/http.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import cache from '../common/cache.js';
+import { serialize } from '../common/util.js';
+
+// 数据缓存时是否需要clone
+const isClone = true;
 
 // 主方法
 function http(method, url, ...arg) {
@@ -8,7 +12,7 @@ function http(method, url, ...arg) {
 
     // get请求优先使用缓存
     if(method === 'get') {
-      let data = cache.get(url);
+      let data = cache.get(url, isClone);
       if(data) {
         return resolve(data);
       }
@@ -17,26 +21,35 @@ function http(method, url, ...arg) {
     // 发起http请求，成功后修改promise状态
     Vue.http[method](url, ...arg).then(function(rep) {
       let body = rep.body;
-      method === 'get' && cache.set(url, body);  // 如果是get请求要缓存结果
+      method === 'get' && cache.set(url, body, isClone);  // 如果是get请求要缓存结果
       resolve(body);
     }, reject);
 
   });
 }
 
+
+
 // 添加细分静态方法
 Object.assign(http, {
 
   // GET请求
-  get(url, config) {
-      return this('get', url, config);
+  get(url, config = {}) {
+    let serializeParams = serialize(config.params);
+    delete config.params;
+
+    return this('get', url + serializeParams, config);
   },
 
-  // POST请求
+  // POST请求，默认把数据转换为formdata格式发送
   post(url, data = {}, config = {}) {
-    let defConfig = {emulateJSON: true};  // 默认数据格式为formdata
+    let serializeParams = serialize(config.params);
+    delete config.params;
+
+    let defConfig = {emulateJSON: true};
     Object.assign(defConfig, config);
-    return this('post', url, data, defConfig);
+
+    return this('post', url + serializeParams, data, defConfig);
   }
 
 });
